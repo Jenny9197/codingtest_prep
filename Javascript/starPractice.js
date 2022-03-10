3242995afa7ec0f7023c59e3b6e995b9632e87c2
Sung Hyun Hong (Jenny) <passionnhope0110@gmail.com>
2022-03-10 22:13:27 +0900

    [js] printstar

---
+++ Javascript/starPractice.js
@@ -0,0 +1,30 @@
+
+*
+**
+***
+****
+*****
+
+for (let i = 0; i < 5; i++) {
+    console.log('*'.repeat(i))
+}
+
+*****
+****
+***
+**
+*
+
+for (let i = 0; i < 5; i++){
+    console.log('*'.repeat(5-i))
+}
+
+    *
+   ***
+  *****
+ *******
+**********
+
+for (let i = 1; i <= 10; i++){
+    console.log('*'.repeat(i+2))
+}
